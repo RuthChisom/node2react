95b01466c98a85d176b2940f364bad9b61ff5ec9
Ruth Chisom <ndukwe100805043@gmail.com>
2022-09-27 13:48:53 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(cors());  // this fixes No 'Access-Control-Allow-Origin' header issue
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
